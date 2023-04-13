e06b309cec3d1e9e8cfe261e29f399521be312ba
ilyapashayan <ilya.w.a.j@gmail.com>
2023-04-13 23:25:30 +0400

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import algoliasearch from 'algoliasearch';
+// import {  useLocation } from 'react-router-dom';
+import { Configure, SortBy, RefinementList, Highlight, Hits, InstantSearch, Pagination, SearchBox, } from 'react-instantsearch-dom';
+// import { useEffect , useState} from 'react';
+
+const searchClient = algoliasearch(
+  'AIWCS83VN4',
+  '5e3936efe97626d6fd2a642c39acec0b',
+  'ukpt_proddefault_products'
+);
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <InstantSearch searchClient={searchClient} >
+          
+
+          <div className='main' >
+            <div>Search
+              <SearchBox />
+              <SortBy
+                defaultRefinement="ukpt_proddefault_products"
+                items={[
+                  { value: 'ukpt_proddefault_products', label: 'Featured' },
+                  { value: 'ukpt_proddefault_products_price_asc', label: 'Price asc.' },
+                  { value: 'ukpt_proddefault_products_price_desc', label: 'Price desc.' },
+                ]}
+              />
+              <RefinementList attribute="manufacturer" />
+            </div>
+
+            <div>
+              <Configure hitsPerPage={8} />
+              <div className='Products'>
+                <Hits hitComponent={Product} />
+              </div>
+              <Pagination showFirst={false} showLast={false} />
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
     </div>
+  );
+}
+
+function Product({ hit }) {
+
+  console.log(hit)
+
+  return (
+
+    <div className='product'>
+      <a href={hit.url}>
+        <Highlight attribute="name" hit={hit} />
+        <img src={hit.image_url} align="left" alt={hit.name} />
+        <p>{hit.price?.GBP.default_formated}</p>
+      </a>
+    </div>
+
   );
 }
 
