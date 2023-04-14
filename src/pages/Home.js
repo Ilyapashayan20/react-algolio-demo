97edb0c3f410e7b40be58ae12491637c196b6eac
ilyapashayan <ilya.w.a.j@gmail.com>
2023-04-14 18:17:34 +0400

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,79 +1,97 @@
 import algoliasearch from 'algoliasearch';
-import { Link } from 'react-router-dom';
-import React from 'react';
+import {  Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
-// import {  useLocation } from 'react-router-dom';
-import { Configure, NumericMenu, SortBy, RefinementList, Highlight, Hits, InstantSearch, Pagination, SearchBox, } from 'react-instantsearch-dom';
-import { useEffect, useState } from 'react';
+import {
+  Configure,
+  Highlight,
+  Hits,
+  InstantSearch,
+  NumericMenu,
+  Pagination,
+  RefinementList,
+  SearchBox,
+  SortBy,
+} from 'react-instantsearch-dom';
 
 const searchClient = algoliasearch(
   'AIWCS83VN4',
-  '5e3936efe97626d6fd2a642c39acec0b',
-  'ukpt_proddefault_products'
+  'f8c9f3dd411497369fc417531c56fad7',
 );
 
 function Home() {
-
   const [index, setIndex] = useState(null);
-
 
   useEffect(() => {
     const initIndex = async () => {
-      const algoliaIndex = searchClient.initIndex('ukpt_proddefault_products');
+       const algoliaIndex =  searchClient.initIndex('ukpt_proddefault_products');
       setIndex(algoliaIndex);
     };
     initIndex();
-  }, [searchClient]);
+  }, []);
 
   useEffect(() => {
     if (index) {
       index.setSettings({
-        searchableAttributes: ['categories', 'price', 'drill_type', 'barcode', 'type'],
+        searchableAttributes: [
+          'categories',
+          'price',
+          'manufacture',
+          'drill_type',
+          'barcode',
+          'type',
+        ],
       });
     }
   }, [index]);
 
-  console.log(index)
-
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
       <div>
-        <InstantSearch searchClient={searchClient} indexName="ukpt_proddefault_products" >
+        <InstantSearch searchClient={searchClient} indexName="ukpt_proddefault_products">
+          <div className="main">
 
-
-          <div className='main' >
-            <div>Search
+            <div>
+              Search
               <SearchBox />
               <SortBy
                 defaultRefinement="ukpt_proddefault_products"
                 items={[
                   { value: 'ukpt_proddefault_products', label: 'Featured' },
-                  { value: 'ukpt_proddefault_products_price_asc', label: 'Price asc.' },
-                  { value: 'ukpt_proddefault_products_price_desc', label: 'Price desc.' },
+
                 ]}
               />
               <NumericMenu
                 attribute="price"
-                items={[
-                  { label: 'All' },
-
-                ]}
+                items={[{ label: 'All' }]}
               />
+              <div>
+                <h3>categories</h3>
               <RefinementList attribute="categories.level0" />
-              <hr></hr>
+              </div>
+              <div>
+                <h3>Drill Type</h3>
               <RefinementList attribute="drill_type" />
-              <hr></hr>
+              </div>
+              <div>
+                <h3>Manufacture</h3>
+              <RefinementList attribute="manufacture" />
+              </div>
+              <div>
+              <h3>Barcode</h3>
               <RefinementList attribute="barcode" />
-              <hr></hr>
+              </div>
+              <div>
+                <hr />
+              <h3>Type</h3>
               <RefinementList attribute="type" />
-
+              </div>
             </div>
 
             <div>
               <Configure hitsPerPage={8} />
-              <div className='Products'>
+              <div className="Products">
                 <Hits hitComponent={Product} />
               </div>
               <Pagination showFirst={false} showLast={false} />
@@ -87,15 +105,13 @@ function Home() {
 
 function Product({ hit }) {
   return (
-
-    <div className='product'>
+    <div className="product">
       <Link to={`/product/${hit.objectID}`}>
         <Highlight attribute="name" hit={hit} />
         <img src={hit.image_url} align="left" alt={hit.name} />
         <p>{hit.price?.GBP.default_formated}</p>
       </Link>
     </div>
-
   );
 }
 
